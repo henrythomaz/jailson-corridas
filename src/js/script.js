7f2def1cf345e89e2932d6c58310911caeb10eb9
Henry Thomaz <henrytoliveriacampos@gmail.com>
2025-04-17 09:03:30 -0400

    passando os arquivos base do jogo

---
+++ src/js/script.js
@@ -0,0 +1,15 @@
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+
+const init = () => {
+
+}
+
+const draw = () => {
+
+}
+
+const update = () => {
+    
+}
